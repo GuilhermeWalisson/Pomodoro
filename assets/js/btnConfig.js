a12453acd47152bc24bee18f9fa1e384906d27ad
Guilhermewal10-10 <guilhermewalison10@gmail.com>
2023-07-29 22:35:33 -0300

    New function

--- assets/js/btnConfig.js
+++ assets/js/btnConfig.js
@@ -1,17 +1,27 @@
-const btnConfig = document.getElementById("btn-config");
+const buttonConfig = document.getElementById("btn-config");
 const timeCount = document.getElementById("timeCount");
 const configContainer = document.getElementById("config-container");
+const btnBack = document.getElementById("btnBack");
 
 let mainState = false;
 
-btnConfig.addEventListener('click', () => {
+buttonConfig.addEventListener('click', () => {
     if(mainState == false) {
         timeCount.style.display = 'none';
         configContainer.style.display = 'flex';
+        btnBack.style.display = 'flex';
         mainState = true;
     } else {
         timeCount.style.display = 'block';
         configContainer.style.display = 'none';
+        btnBack.style.display = 'none';
         mainState = false;
     }
 });
+
+btnBack.addEventListener('click', () => {
+    timeCount.style.display = 'block';
+    configContainer.style.display = 'none';
+    btnBack.style.display = 'none';
+    mainState = false;
+})
