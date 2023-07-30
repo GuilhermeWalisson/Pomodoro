0e931aa945cee7229b2b5dbc7ae25030fded719d
Guilhermewal10-10 <guilhermewalison10@gmail.com>
2023-07-29 22:35:12 -0300

    New feature

--- assets/js/btnColorConfig.js
+++ assets/js/btnColorConfig.js
@@ -1,38 +1,22 @@
-const btnColorDefault = document.getElementById("btn-bg-default");
-const btnColorRed = document.getElementById("btn-bg-red");
-const btnColorPurple = document.getElementById("btn-bg-purple");
-const btnColorDark = document.getElementById("btn-bg-dark");
-const btnColorOrange = document.getElementById("btn-bg-orange");
 const body = document.getElementById("body");
 
-let color;
+const colors = {
+    red: 'rgb(255, 55, 55)',
+    default: '#03c988',
+    purple: 'rgb(141, 99, 255)',
+    dark: 'rgba(0, 0, 0, 0.89)',
+    orange: 'rgb(255, 174, 99)'
+};
 
-btnColorDefault.addEventListener('click', () =>{
-    color = '#03c988'
-    body.style.background = color;
-    localStorage.setItem('bodyColor', color)
-});
+function bodyBgColorEdit(btnId, color) {
+    document.getElementById(btnId).addEventListener('click', () => {
+        body.style.background = color;
+        localStorage.setItem('bodyColor', color);
+    })
+};
 
-btnColorRed.addEventListener('click', () =>{
-    color = 'rgb(255, 55, 55)'
-    body.style.background = color;
-    localStorage.setItem('bodyColor', color)
-});
-
-btnColorPurple.addEventListener('click', () =>{
-    color = 'rgb(141, 99, 255)'
-    body.style.background = color;
-    localStorage.setItem('bodyColor', color)
-});
-
-btnColorDark.addEventListener('click', () =>{
-    color = 'rgba(0, 0, 0, 0.89)'
-    body.style.background = color;
-    localStorage.setItem('bodyColor', color)
-});
-
-btnColorOrange.addEventListener('click', () =>{
-    color = 'rgb(255, 174, 99)'
-    body.style.background = color;
-    localStorage.setItem('bodyColor', color)
-});
+bodyBgColorEdit('btn-bg-default', colors.default);
+bodyBgColorEdit('btn-bg-red', colors.red);
+bodyBgColorEdit('btn-bg-purple', colors.purple);
+bodyBgColorEdit('btn-bg-dark', colors.dark);
+bodyBgColorEdit('btn-bg-orange', colors.orange);
